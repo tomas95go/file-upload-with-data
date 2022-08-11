57ac6cb1de3ef69f33df5c12d38b353ed221d518
tomas95go <tomas95go@gmail.com>
2022-08-11 10:39:16 -0300

    Validando datos ANTES de subir la img

--- server.js
+++ server.js
@@ -1,17 +1,38 @@
 const express = require("express");
 const app = express();
 const multer = require("multer");
-const upload = multer({ dest: "./public" });
+function fileFilter(req, file, cb) {
+  const userExists = true;
+  if (userExists) {
+    req.fileValidationError = "No anda amego";
+    return cb(null, false, new Error("No anda"));
+  }
+  cb(null, true);
+}
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./public");
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, file.fieldname + "-" + uniqueSuffix);
+  },
+});
+const upload = multer({ storage: storage, fileFilter: fileFilter }).single(
+  "user-avatar"
+);
 const PORT = process.env.PORT || 5000;
 
 app.use(express.static("public"));
 app.use(express.json());
 
-app.post("/", upload.single("user-avatar"), function (req, res, next) {
-  const { "user-input": user, "user-password": password } = req.body;
-  console.log(user, password, req.file);
-  res.status(200).json({
-    message: "Esta bien su codigo sr. :)",
+app.post("/", function (req, res, next) {
+  upload(req, res, function (err) {
+    if (req.fileValidationError) {
+      return res.status(200).json({
+        message: req.fileValidationError,
+      });
+    }
   });
 });
 
